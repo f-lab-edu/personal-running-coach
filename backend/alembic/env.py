791ca2b9a97c808b402b9cd8e278059bf21dd450
jaykim <jaykim.coding@gmail.com>
2025-10-11 13:23:19 +0900

    alembic 로컬환경 개발 세팅

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,10 +5,22 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+env_path = Path(__file__).resolve().parent / "src" / ".env"
+load_dotenv(env_path)
+
+# url = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@db:5432/mydb")
+# if url.startswith("postgresql+asyncpg"):
+#     url = url.replace("postgresql+asyncpg", "postgresql+psycopg2") # alembic 에서는 sync로 마이그레이션
+
+url = "postgresql+psycopg2://user:password@localhost:5432/mydb"
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_main_option("sqlalchemy.url", url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -16,16 +28,14 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+# sqlmodel 
+from sqlmodel import SQLModel
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
